9d0d456b375e5e02783a96fc6646864426f8fb3d
Andy Sarroff <sarroff@cs.dartmouth.edu>
2013-06-23 01:07:59 -0400

    fixed error with converting long long type to string. Chnged probe frequency to 1000 Hz

--- demos/ipconnectProbe.d
+++ demos/ipconnectProbe.d
@@ -1,6 +1,6 @@
 /* Adapted from soconnect.d from http://www.dtracebook.com/ */
 #pragma D option quiet
-#pragma D option switchrate=10000hz
+#pragma D option switchrate=1000hz
 
 inline int af_inet = 2;		/* AF_INET defined in bsd/sys/socket.h */
 inline int af_inet6 = 30;	/* AF_INET6 defined in bsd/sys/socket.h */
@@ -18,13 +18,13 @@ syscall::connect*:entry
 	/* Convert port to host byte order without ntohs() being available. */
 	self->port = (this->s->sin_port & 0xFF00) >> 8;
 	self->port |= (this->s->sin_port & 0xFF) << 8;
-        self->start = 1;
+    self->start = 1;
 }
 
 syscall::connect*:return
 /self->start/
 {
-	printf("ipconnect %d", self->port);
+	printf("ipconnect %s", lltostr(self->port));
 	self->port = 0;
 	self->start = 0;
 }
